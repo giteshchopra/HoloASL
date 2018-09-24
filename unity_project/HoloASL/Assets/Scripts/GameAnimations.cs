42bb2d8a406a9cc7b0ccbc1e3f6786eff35f2da5
Medha Srivastava <srivastava.medha11@gmail.com>
2018-09-23 22:07:16 -0700

    -Added animation for alphabet 'L' - Attempted playing one animation after the other. - Following are the current issues: 	- Animations are getting played alternately 	- Cannot find a way to play another animation only after first 	completes

--- unity_project/HoloASL/Assets/Scripts/GameAnimations.cs
+++ unity_project/HoloASL/Assets/Scripts/GameAnimations.cs
@@ -8,7 +8,7 @@ public class GameAnimations : MonoBehaviour {
     /// </summary>
     public static GameAnimations Instance;
 
-    internal Animator wordAnimation;
+    internal Animator WordAnimation;
 
     /// <summary>
     /// Called on Initialization
@@ -21,13 +21,23 @@ public class GameAnimations : MonoBehaviour {
     void Start () {
 
         //Fetch the Animator from GameObject
-        wordAnimation = GetComponent<Animator>();
+        WordAnimation = GetComponent<Animator>();
     }
 
     public void PlayAnimation()
     {
         // wordAnimation["AlpF"].wrapMode = WrapMode.Once;
-        wordAnimation.Play("AlpF");
+        WordAnimation.Play("AlpF");
+        Wait(5);
+        WordAnimation.Play("AlpL");
+    }
+    IEnumerator Wait(float duration)
+    {
+        //This is a coroutine
+        Debug.Log("Start Wait() function. The time is: " + Time.time);
+        Debug.Log("Float duration = " + duration);
+        yield return new WaitForSecondsRealtime(duration);   //Wait
+        Debug.Log("End Wait() function and the time is: " + Time.time);
     }
     // Update is called once per frame
     void Update () {
